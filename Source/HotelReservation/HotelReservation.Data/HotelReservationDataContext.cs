b3693c7858c1049c9b46e624e4b8815cf6291239
Rasika Weliwita <rasika.weliwita@cslworld.com>
2015-03-11 06:01:23 +0530

    initial commit

---
+++ Source/HotelReservation/HotelReservation.Data/HotelReservationDataContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservation.Data
+{
+    public class HotelReservationDataContext : DbContext
+    {
+        public HotelReservationDataContext()
+            : base("HotelReservationDataContext")
+        { }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Changing Database table name to Metadata
+            modelBuilder.Entity<Customer>()
+                .ToTable("tblCustomers");
+        }
+    }
+}
